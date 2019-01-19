f6e4400e34e18ed9db821b541a4bb341669d4234
Brandon Zupan <brandonzforty2@gmail.com>
2019-01-19 11:22:36 -0600

    Began mute command

--- BotCode/AdministrationModules.cs
+++ BotCode/AdministrationModules.cs
@@ -9,7 +9,48 @@ using Discord.WebSocket;
 
 namespace BotCode.Modules
 {
-    class Administration : ModuleBase
+    public class Administration : ModuleBase
     {
+        // ~say hello -> hello
+        [Command("say"), Summary("Echos a message.")]
+        public async Task Say([Remainder, Summary("The text to echo")] string echo)
+        {
+            // ReplyAsync is a method on ModuleBase
+            var user = Context.User as SocketGuildUser;
+            var permission = (user as IGuildUser).GuildPermissions;
+            if (permission.Administrator == true)
+            {
+                await ReplyAsync(echo);
+            }
+            else if (user.Id == 239099905849819137)
+            {
+                await ReplyAsync("Mox please stop messing with stuff");
+            }
+            else
+            {
+                await ReplyAsync("You do not have permission to do that");
+            }
+
+        }
+
+        [Command("mute"), Summary("Mutes a person, sends them a warning, and logs the infraction")]
+        public async Task Mute([Remainder, Summary("~mute [user] [reason]")] string mute)
+        {
+            var user = Context.User as SocketGuildUser;
+            var permission = (user as IGuildUser).GuildPermissions;
+            if (permission.Administrator == true)
+            {
+                await ReplyAsync("yeet");
+            }
+            else if (user.Id == 239099905849819137)
+            {
+                await ReplyAsync("Mox please stop messing with stuff");
+            }
+            else
+            {
+                await ReplyAsync("You do not have permission to do that");
+            }
+            
+        }
     }
 }
