0856e28296e9b1453cee5e2013e1da7524955751
Brandon Zupan <brandonzforty2@gmail.com>
2019-01-19 11:03:36 -0600

    Added file for admin commands

---
+++ BotCode/AdministrationModules.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Discord;
+using Discord.WebSocket;
+
+namespace BotCode.Modules
+{
+    class Administration : ModuleBase
+    {
+    }
+}
